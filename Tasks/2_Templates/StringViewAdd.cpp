e6b43fad7830cd877b5f52ccd24a19b6b68a4abf
jakjan95 <jakubjanusz95@gmail.com>
2021-09-21 23:25:31 +0200

    StringViewAdd done

--- Tasks/2_Templates/StringViewAdd.cpp
+++ Tasks/2_Templates/StringViewAdd.cpp
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <string>
 #include <string_view>
+#include <type_traits>
 
 
 std::string operator+( std::string_view lhs, std::string_view rhs )
@@ -23,13 +24,18 @@ std::string operator+( std::string_view lhs, std::string_view rhs )
    return std::string( lhs ) + std::string( rhs );
 }
 
-
-template< typename T >
-T add( const T& lhs, const T& rhs )
+template <typename T>
+auto add(const T& lhs, const T& rhs)
 {
-   return lhs + rhs;
+    return lhs + rhs;
 }
 
+/*
+/ We are returning string_view which is constructed based on string
+/ At point when we are returning it we return freed stuff which cause danling pointer to freed table
+/ The reason for that is string_view which is non-owning view :) 
+/ Solution can be auto which deduce return type as std::string
+*/
 
 int main()
 {
@@ -40,6 +46,7 @@ int main()
    std::string_view sv2{ s2 };
 
    const auto result = add( sv1, sv2 );
+   std::cout << typeid(result).name() <<'\n';
 
    std::cout << "\n result = " << result << "\n\n";
 
