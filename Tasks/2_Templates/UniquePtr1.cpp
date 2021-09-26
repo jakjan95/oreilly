9f26a3087738cb55aa2c13270ec1fb6c00866f77
jakjan95 <jakubjanusz95@gmail.com>
2021-09-26 16:41:17 +0200

    UniquePtr1 done

--- Tasks/2_Templates/UniquePtr1.cpp
+++ Tasks/2_Templates/UniquePtr1.cpp
@@ -21,6 +21,7 @@
 #include <cstddef>
 #include <cstdlib>
 #include <iostream>
+#include <memory>
 #include <utility>
 
 
@@ -53,51 +54,180 @@ class Widget
    int i_{ 0 };
 };
 
-
-
-
 //---- <memory> -----------------------------------------------------------------------------------
 
-// Step 1: Implement the 'unique_ptr' base template for single memory resources
-// Step 2: Add a template parameter for a deleter
-template< typename T >
-class unique_ptr
-{
- public:
-   using pointer = T*;
-   using element_type = T;
-
-   // Step 1: Think about the following functions:
-   //  - default constructor
-   //  - constructor taking a pointer to a dynamic resource
-   //  - destructor
-
-   // Step 4: Implement the copy and move operations:
-   //  - copy constructor
-   //  - move constructor
-   //  - copy assignment operator
-   //  - move assignment operator
-
-   // Step 5: Implement move operations for different pointer types:
-   //  - move constructor for different pointer types
-   //  - move assignment operator for different pointer types
-
-   // Step 6: Implement the 'reset()' and 'release()' functions
-
-   T& operator*()  const { return *ptr_; }
-   T* operator->() const { return ptr_;  }
-
- private:
-   T* ptr_;
+template <typename T>
+struct my_defaul_deleter {
+    void operator()(T* resources)
+    {
+        delete resources;
+    }
 };
 
+template <typename T, typename D = my_defaul_deleter<T>>
+class unique_ptr {
+public:
+    using pointer = T*;
+    using element_type = T;
 
+    constexpr unique_ptr()
+        : ptr_ { nullptr }
+    {
+    }
 
+    explicit unique_ptr(T* ptr)
+        : ptr_ { ptr }
+    {
+    }
+
+    ~unique_ptr()
+    {
+        D {}(ptr_);
+    }
+
+    unique_ptr(const unique_ptr&) = delete;
+
+    unique_ptr(unique_ptr&& other_ptr) noexcept
+        : ptr_ { other_ptr.ptr_ }
+    {
+
+        other_ptr.ptr_ = nullptr;
+    }
+    template <typename U, typename E>
+    unique_ptr(unique_ptr<U, E>&& other_ptr) noexcept
+        : ptr_ { other_ptr.ptr_ }
+    {
+
+        other_ptr.ptr_ = nullptr;
+    }
+
+    unique_ptr& operator=(const unique_ptr&) = delete;
+
+    unique_ptr& operator=(unique_ptr&& other_ptr) noexcept
+    {
+        D {}(ptr_);
+        ptr_ = other_ptr.ptr_;
+        other_ptr.ptr_ = nullptr;
+        return *this;
+    }
+
+    template <typename U, typename E>
+    unique_ptr& operator=(unique_ptr<U, E>&& other_ptr) noexcept
+    {
+        D {}(ptr_);
+        ptr_ = other_ptr.ptr_;
+        other_ptr.ptr_ = nullptr;
+        return *this;
+    }
+
+    T* release() noexcept
+    {
+        T* tmp = std::move(ptr_);
+        ptr_ = nullptr;
+        return tmp;
+    }
+
+    void reset(T* new_ptr = nullptr) noexcept
+    {
+        D {}(ptr_);
+        ptr_ = new_ptr;
+    }
+
+    T& operator*() const { return *ptr_; }
+    T* operator->() const { return ptr_; }
+
+private:
+    T* ptr_;
+
+    template <typename U, typename E>
+    friend class unique_ptr;
+};
 
 // Step 3: Implement a 'unique_ptr' specialization for arrays
+template <typename T>
+struct my_defaul_deleter<T[]> {
 
+    template <typename U>
+    void operator()(U* resources)
+    {
+        delete[] resources;
+    }
+};
 
+template <typename T, typename D>
+class unique_ptr<T[], D> {
+public:
+    using pointer = T*;
+    using element_type = T;
 
+    constexpr unique_ptr()
+        : ptr_ { nullptr }
+    {
+    }
+
+    explicit unique_ptr(T* ptr)
+        : ptr_ { ptr }
+    {
+    }
+
+    ~unique_ptr()
+    {
+        D {}(ptr_);
+    }
+
+    unique_ptr(const unique_ptr&) = delete;
+
+    unique_ptr(unique_ptr&& other_ptr) noexcept
+        : ptr_ { other_ptr.ptr_ }
+    {
+
+        other_ptr.ptr_ = nullptr;
+    }
+    template <typename U, typename E>
+    unique_ptr(unique_ptr<U, E>&& other_ptr) noexcept
+        : ptr_ { other_ptr.ptr_ }
+    {
+
+        other_ptr.ptr_ = nullptr;
+    }
+
+    unique_ptr& operator=(const unique_ptr&) = delete;
+
+    unique_ptr& operator=(unique_ptr&& other_ptr) noexcept
+    {
+        D {}(ptr_);
+        ptr_ = other_ptr.ptr_;
+        other_ptr.ptr_ = nullptr;
+        return *this;
+    }
+
+    template <typename U, typename E>
+    unique_ptr& operator=(unique_ptr<U, E>&& other_ptr) noexcept
+    {
+        D {}(ptr_);
+        ptr_ = other_ptr.ptr_;
+        other_ptr.ptr_ = nullptr;
+        return *this;
+    }
+
+    T* release() noexcept
+    {
+        T* tmp = std::move(ptr_);
+        ptr_ = nullptr;
+        return tmp;
+    }
+
+    void reset(T* new_ptr = nullptr) noexcept
+    {
+        D {}(ptr_);
+        ptr_ = new_ptr;
+    }
+
+    T& operator[](size_t index) const { return ptr_[index]; }
+
+private:
+    T* ptr_;
+};
 
 //---- <Main.cpp> ---------------------------------------------------------------------------------
 
@@ -106,30 +236,29 @@ int main()
    // unique_ptr for a single Widget
    {
       // Step 1+2
-      /*
+      
       auto a = unique_ptr<Widget>( new Widget(2) );
       std::cout << " a has been created (a=" << (*a).get() << ")\n\n";
 
       auto b = unique_ptr<Widget>( new Widget(3) );
       std::cout << " b has been created (b=" << b->get() << ")\n\n";
-      */
+      
 
       // Step 4+5
-      /*
-      //a = b;  // Compilation error!!!
+      
+      // a = b;  // Compilation error!!!
 
       unique_ptr<const Widget> c( std::move( a ) );
       std::cout << " a has been moved to c (c=" << c->get() << ")\n\n";
 
       a = std::move( b );
       std::cout << " b has been moved to a (a=" << a->get() << ")\n\n";
-      */
    }
 
    // unique_ptr for an array of Widgets
    {
       // Step 3
-      /*
+      
       auto a = unique_ptr<Widget[]>( new Widget[2] );
       a[0].set( 1 );
       a[1].set( 2 );
@@ -140,10 +269,10 @@ int main()
       b[1].set( 4 );
       b[2].set( 5 );
       std::cout << " b has been created (a=[" << b[0].get() << "," << b[1].get() << "," << b[2].get() << "])\n\n";
-      */
+      
 
       // Step 4+5
-      /*
+      
       //a = b;  // Compilation error!!!
 
       unique_ptr<Widget[]> c( std::move( a ) );
@@ -151,7 +280,7 @@ int main()
 
       a = std::move( b );
       std::cout << " b has been moved to a (a=[" << a[0].get() << "," << a[1].get() << "," << a[2].get() << "])\n\n";
-      */
+      
    }
 
    return EXIT_SUCCESS;
