09878e75aa871989701573a2dd7966a2969b4dee
jakjan95 <jakubjanusz95@gmail.com>
2023-08-22 06:51:28 +0200

    Print - variadic templates introduced

--- Tasks/2_Templates/Print.cpp
+++ Tasks/2_Templates/Print.cpp
@@ -20,18 +20,42 @@
 
 // TODO: Extend the given 'print()' function by variadic templates to enable an arbitrary
 //       number of function arguments.
-template< typename T >
-void print( std::ostream& os, const T& value )
+// void print(std::ostream&) // (1)
+// {
+// }
+
+/*
+//Multiple instantiation for different numbers of parameters
+template<typename T, typename... Ts > // T is parameter pack
+void print( std::ostream& os, const T& value, const Ts&... values ) // (2)
 {
    os << value;
+   if constexpr(sizeof...(Ts)>0U){ // if it is false, the compiler doesn't peak inside it :) C++17
+      print(os, values...); // Tail recursion
+   }
 }
+*/
 
+//third solution with at least one parameter:
+// template <typename T, typename... Ts> // T is parameter pack
+// void print(std::ostream& os, const T& value, const Ts&... values) // (2)
+// {
+//     os << value;
+//     (os << ... << values); // Fold expression (C++17) -> only for operators
+// }
+
+// Instantiation just once
+template <typename... Ts> // T is parameter pack
+void print(std::ostream& os, const Ts&... values) // (2)
+{
+   (os << ... << values); // Fold expression (C++17) -> only for operators
+}
 
 int main()
 {
-   //print( std::cout, "Hello\n" );
-   //print( std::cout, "Two ", "words\n" );
-   //print( std::cout, "Three ", "different ", "words\n" );
+   print( std::cout, "Hello\n" ); // C-style string literal: char[7]
+   print( std::cout, "Two ", "words\n" ); //char[5], char[7] ...
+   print( std::cout, "Three ", "different ", "words\n" );
    //print( std::cout, "Numbers: ", 1, ' ', 1.2, ' ', '\n' );
 
    return EXIT_SUCCESS;
