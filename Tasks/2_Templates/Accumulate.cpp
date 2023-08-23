8ed3cc550a531f196c47f52ec911d3b50b5101b3
jakjan95 <jakubjanusz95@gmail.com>
2023-08-23 07:32:00 +0200

    Accumulate implemented

--- Tasks/2_Templates/Accumulate.cpp
+++ Tasks/2_Templates/Accumulate.cpp
@@ -26,7 +26,9 @@
 #include <cstdlib>
 #include <functional>
 #include <iostream>
+#include <iterator>
 #include <list>
+#include <utility>
 #include <vector>
 
 
@@ -34,28 +36,55 @@
 //         an initial value for the reduction operation, and a binary operation that performs the
 //         elementwise reduction (see https://en.cppreference.com/w/cpp/algorithm/accumulate,
 //         overload (2)).
-// TODO
+template <typename Iterator, typename T, typename Operation>
+constexpr T accumulate(Iterator first, Iterator last, T init, Operation op)
+{
+    while (first != last) {
+        init = op(std::move(init), *first++);
+    }
+
+    return init;
+}
 
 // Step 2: Implement an overload of the 'accumulate()' algorithm that uses 'std::plus' as the
 //         default binary operation.
-// TODO
+template <typename Iterator, typename T>
+constexpr T accumulate(Iterator first, Iterator last, T init)
+{
+    return accumulate(first, last, init, std::plus<> {});
+}
 
 // Step 3: Implement an overload of the 'accumulate()' algorithm that uses the default of the
 //         underlying data type as initial value and 'std::plus' as the default binary operation.
-// TODO
+template <typename Iterator>
+constexpr auto accumulate(Iterator first, Iterator last)
+{
+    //  using IterType = decltype(*first);
+    using ValueType = typename std::iterator_traits<Iterator>::value_type;
+    using ReturnType = decltype(std::declval<ValueType>() + std::declval<ValueType>());
+    return accumulate(first, last, ReturnType {});
+}
 
 // Step 4: Test your implementation with a custom binary operation (e.g. 'Times').
-// TODO
+struct Times {
 
+    template <typename T1, typename T2>
+    constexpr auto operator()(const T1& lhs, const T2& rhs)
+    {
+        return lhs * rhs;
+    }
+};
 
 int main()
 {
-   /*
    // Sum of values in a vector of integral values
    {
       const std::vector<int> v{ 1, 3, 5, 7 };
-      const auto sum = accumulate( begin(v), end(v), int{0}, std::plus<>{} );
-      std::cout << "\n sum = " << sum << "\n\n";
+      const auto sum1 = accumulate( begin(v), end(v), int{0}, std::plus<>{} );
+      std::cout << "\n sum1 = " << sum1 << "\n\n";
+      const auto sum2 = accumulate( begin(v), end(v) );
+      std::cout << "\n sum2 = " << sum2 << "\n\n";
+
    }
 
    // Product of values in a list of floating-point values
@@ -64,7 +93,6 @@ int main()
       const auto sum = accumulate( begin(l), end(l), double{1}, Times{} );
       std::cout << "\n sum = " << sum << "\n\n";
    }
-   */
 
    return EXIT_SUCCESS;
 }
