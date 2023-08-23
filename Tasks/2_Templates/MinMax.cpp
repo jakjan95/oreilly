2ffed495dac25d04211b0c487416e131266cc83d
jakjan95 <jakubjanusz95@gmail.com>
2023-08-23 06:47:18 +0200

    MinMax - function templates implemented

--- Tasks/2_Templates/MinMax.cpp
+++ Tasks/2_Templates/MinMax.cpp
@@ -27,24 +27,60 @@
 
 
 // Step 1: Definition of the 'minmax()' function template with a single template argument
-// TODO
-
+template <typename T>
+std::pair<T, T> minmax(const T& lhs, const T& rhs)
+{
+    if (lhs < rhs) {
+        return std::make_pair(lhs, rhs);
+    } else {
+        return std::make_pair(rhs, lhs);
+    }
+}
 
 // Step 2: Definition of the 'minmax()' function template with a two template arguments
-// TODO
-
+template <typename T1, typename T2>
+auto minmax(const T1& lhs, const T2& rhs) -> std::pair<std::common_type_t<T1, T2>, std::common_type_t<T1, T2>>
+{
+    if (lhs < rhs) {
+        return std::make_pair(lhs, rhs);
+    } else {
+        return std::make_pair(rhs, lhs);
+    }
+}
 
 // Step 3: Overload of the 'minmax()' function
-// TODO
+std::pair<int, int> minmax(const int& lhs, const int& rhs)
+{
+    if (lhs < rhs) {
+        return std::make_pair(lhs, rhs);
+    } else {
+        return std::make_pair(rhs, lhs);
+    }
+}
 
 
 // Step 4: Specialization of the 'minmax()' function template with a single template argument
-// TODO
+template <>
+std::pair<int, int> minmax<int>(const int& lhs, const int& rhs)
+{
+    if (lhs < rhs) {
+        return std::make_pair(lhs, rhs);
+    } else {
+        return std::make_pair(rhs, lhs);
+    }
+}
 
 
 // Step 4: Specialization of the 'minmax()' function template with two template arguments
-// TODO
-
+template <>
+std::pair<int,int> minmax<int,int>(const int& lhs, const int& rhs)
+{
+    if (lhs < rhs) {
+        return std::make_pair(lhs, rhs);
+    } else {
+        return std::make_pair(rhs, lhs);
+    }
+}
 
 // Auxiliary helper function to print 'std::pair'
 template< typename T1, typename T2 >
@@ -57,10 +93,10 @@ std::ostream& operator<<( std::ostream& os, std::pair<T1,T2> const& p )
 int main()
 {
    std::cout << "\n"
-                //" minmax( 1, 5 )    = " << minmax( 1, 5 ) << "\n"
-                //" minmax( 3, 2 )    = " << minmax( 3, 2 ) << "\n"
-                //" minmax( 1.2, -4 ) = " << minmax( 1.2, -4 ) << "\n"
-                //" minmax( 1, 2.8 )  = " << minmax( 1, 2.8 ) << "\n"
+                " minmax( 1, 5 )    = " << minmax( 1, 5 ) << "\n"
+                " minmax( 3, 2 )    = " << minmax( 3, 2 ) << "\n"
+                " minmax( 1.2, -4 ) = " << minmax( 1.2, -4 ) << "\n"
+                " minmax( 1, 2.8 )  = " << minmax( 1, 2.8 ) << "\n"
                 "\n";
 
    return EXIT_SUCCESS;
