dfb9536ee3844f899686d14c1859b678675ba134
jakjan95 <jakubjanusz95@gmail.com>
2023-08-28 19:08:31 +0200

    Partition - implemented parition() and partition_point()

--- Tasks/2_STL_Algorithms/Partition.cpp
+++ Tasks/2_STL_Algorithms/Partition.cpp
@@ -14,6 +14,7 @@
 *
 **************************************************************************************************/
 
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
 #include <list>
@@ -24,14 +25,43 @@
 // Implement the 'partition()' algorithm that separates two groups of elements. The algorithm
 // should take a pair of iterators and a unary predicate that identifies the elements of the
 // first group. (see https://en.cppreference.com/w/cpp/algorithm/partition).
-// TODO
+template <class ForwardIt, class UnaryPredicate>
+ForwardIt partition(ForwardIt first, ForwardIt last, UnaryPredicate p)
+{
+    if (first == last) {
+        return last;
+    }
+    --last;
 
+    while (first != last) {
+        while (first != last && p(*first)) {
+            ++first;
+        }
+
+        while (first != last && !p(*last)) {
+            --last;
+        }
+
+        if (first != last) {
+            std::iter_swap(first, last);
+        }
+    }
+
+    return first;
+}
 
 // Bonus: Implement the 'partition_point()' algorithm, that locates the end of the first
 // partition, that is, the first element that does not satisfy p or last if all elements
 // satisfy p (see https://en.cppreference.com/w/cpp/algorithm/partition_point).
-// TODO
+template <class ForwardIt, class UnaryPredicate>
+ForwardIt partition_point(ForwardIt first, ForwardIt last, UnaryPredicate p)
+{
+    while (first != last && p(*first)) {
+        ++first;
+    }
 
+    return first;
+}
 
 [[nodiscard]] constexpr bool is_odd( int i ) noexcept { return i%2 == 1; }
 [[nodiscard]] constexpr bool is_small( int i ) noexcept { return i < 10; }
@@ -39,7 +69,6 @@
 
 int main()
 {
-   /*
    // Separating odd and even values in a std::vector
    {
       std::vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
@@ -54,6 +83,9 @@ int main()
       for( auto pos=partition_point; pos!=end(v); ++pos ) {
          std::cout << ' ' << *pos;
       }
+      auto partitionPointFromAlgorithm = ::partition_point(v.begin(), v.end(), is_odd);
+      std::cout<<"\nPartition point at: "<< std::distance(v.begin(), partitionPointFromAlgorithm)<<'\n';
+
       std::cout << "\n\n";
    }
 
@@ -71,9 +103,10 @@ int main()
       for( auto pos=partition_point; pos!=end(l); ++pos ) {
          std::cout << ' ' << *pos;
       }
+      auto partitionPointFromAlgorithm = ::partition_point(l.begin(), l.end(), is_small);
+      std::cout<<"\nPartition point at: "<< std::distance(l.begin(), partitionPointFromAlgorithm)<<'\n';
       std::cout << "\n\n";
    }
-   */
 
    return EXIT_SUCCESS;
 }
